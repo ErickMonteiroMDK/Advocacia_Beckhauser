90ef5aa8be2503eba72e5246d506ae286572295b
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2023-10-24 20:52:51 -0300

    criação classe Juridica

--- src/Models/Pessoa/Juridica.java
+++ src/Models/Pessoa/Juridica.java
@@ -1,27 +1,9 @@
 package Models.Pessoa;
 
-public class Juridica extends Pessoa {
+public class Juridica {
     private String cnpj;
-    private String tipoEmpresa;
-
-    public Juridica(String cnpj, String tipoEmpresa) {
-        this.cnpj = cnpj;
-        this.tipoEmpresa = tipoEmpresa;
-    }
 
     public String getCnpj() {
         return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getTipoEmpresa() {
-        return tipoEmpresa;
-    }
-
-    public void setTipoEmpresa(String tipoEmpresa) {
-        this.tipoEmpresa = tipoEmpresa;
     }
 }
