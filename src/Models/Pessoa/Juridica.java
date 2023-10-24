99efe795589ad78fe23a886bb56ea829d02fdd44
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2023-10-24 20:57:52 -0300

    Alterações Classe P. Jurídica.

--- src/Models/Pessoa/Juridica.java
+++ src/Models/Pessoa/Juridica.java
@@ -2,8 +2,15 @@ package Models.Pessoa;
 
 public class Juridica {
     private String cnpj;
+    private String tipoEmpresa;
+
 
     public String getCnpj() {
         return cnpj;
     }
+
+    public String getTipoEmpresa() {
+        return tipoEmpresa;
+    }
+
 }
