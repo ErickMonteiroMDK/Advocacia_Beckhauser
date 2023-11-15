db1fffa5be7e531a23c7ef5eb44e96303f3c0bcf
lucas0headshot <lucas0headshot@gmail.com>
2023-11-15 13:41:29 -0300

    Adição do projeto c/ SpringBoot

---
+++ src/main/java/com/advocacia/Advocacia_Beckhauser/AdvocaciaBeckhauserApplication.java
@@ -0,0 +1,11 @@
+package com.advocacia.Advocacia_Beckhauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvocaciaBeckhauserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AdvocaciaBeckhauserApplication.class, args);
+	}
+}
---
+++ src/test/java/com/advocacia/Advocacia_Beckhauser/AdvocaciaBeckhauserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advocacia.Advocacia_Beckhauser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvocaciaBeckhauserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
