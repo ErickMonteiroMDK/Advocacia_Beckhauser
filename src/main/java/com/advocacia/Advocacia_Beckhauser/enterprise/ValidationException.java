b75e822f4ce83866970271c7d949692d5cfe797b
lucas0headshot <lucas0headshot@gmail.com>
2023-11-18 21:13:40 -0300

    Adição do ValidationException & AbstractController - Lançar e tratar exceções personalizadas
    Agora basta extender o AbstractController nos Controllers e lançar exceções diretamente dos seus respectivos Services

---
+++ src/main/java/com/advocacia/Advocacia_Beckhauser/enterprise/ValidationException.java
@@ -0,0 +1,8 @@
+package com.advocacia.Advocacia_Beckhauser.enterprise;
+
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
