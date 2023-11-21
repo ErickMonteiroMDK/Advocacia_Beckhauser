288a62703151de9b34ba6797fec5c2a04493b6d3
lucas0headshot <lucas0headshot@gmail.com>
2023-11-21 01:11:42 -0300

    Adição relação entre Modelo & Processo - ManyToOne

--- src/main/java/com/advocacia/Advocacia_Beckhauser/models/Modelo.java
+++ src/main/java/com/advocacia/Advocacia_Beckhauser/models/Modelo.java
@@ -1,9 +1,8 @@
 package com.advocacia.Advocacia_Beckhauser.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Lob;
+import javax.persistence.*;
 import java.io.File;
+import java.util.Arrays;
 
 @Entity()
 public class Modelo extends EntityID {
@@ -13,6 +12,11 @@ public class Modelo extends EntityID {
     @Lob
     @Column(nullable = false)
     private byte[] dados;
+
+    @ManyToOne
+    @JoinColumn(name = "processo_id", referencedColumnName = "id")
+    private Processo processo;
+
 
 
     public String getNome() {
@@ -29,5 +33,22 @@ public class Modelo extends EntityID {
 
     public void setDados(byte[] dados) {
         this.dados = dados;
+    }
+
+    public Processo getProcesso() {
+        return processo;
+    }
+
+    public void setProcesso(Processo processo) {
+        this.processo = processo;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Modelo{" +
+                "nome='" + nome + '\'' +
+                ", dados=" + Arrays.toString(dados) +
+                '}';
     }
 }
