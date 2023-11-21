f05111ddfbc7c3abdfb3119b2d80554dc2199705
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2023-11-21 19:38:27 -0300

    Revert "Remoção dos arquivos"
    This reverts commit a9c4f1f4e0568270e0231679eac219994e952042.

---
+++ src/main/java/com/advocacia/Advocacia_Beckhauser/repositories/PessoaJuridicaRepository.java
@@ -0,0 +1,14 @@
+package com.advocacia.Advocacia_Beckhauser.repositories;
+
+import com.advocacia.Advocacia_Beckhauser.models.Cliente;
+import com.advocacia.Advocacia_Beckhauser.models.PessoaJuridica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaJuridicaRepository extends JpaRepository<PessoaJuridica, Long> {
+
+    public PessoaJuridica findByCnpj (String Cnpj);
+
+    public PessoaJuridica findByRazaoSocial (String RazaoSocial);
+
+
+}
---
+++ src/main/java/com/advocacia/Advocacia_Beckhauser/services/PessoaJuridicaService.java
@@ -0,0 +1,59 @@
+package com.advocacia.Advocacia_Beckhauser.services;
+
+import com.advocacia.Advocacia_Beckhauser.enterprise.ValidationException;
+import com.advocacia.Advocacia_Beckhauser.models.PessoaJuridica;
+import com.advocacia.Advocacia_Beckhauser.repositories.PessoaJuridicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PessoaJuridicaService {
+
+    @Autowired
+    private PessoaJuridicaRepository repository;
+
+    public PessoaJuridica salvar(PessoaJuridica entity) {
+
+        if(repository.findByCnpj(entity.getCnpj()) != null) {
+            throw new ValidationException("Este CNPJ já está cadastrado no sistema.");
+        }
+
+        if (repository.findByRazaoSocial(entity.getRazaoSocial()) != null) {
+            throw new ValidationException("Já existe esta razão social cadastrada no sistema. ");
+        }
+
+        return  repository.save(entity);
+    }
+
+    public List<PessoaJuridica> buscaTodos() { return  repository.findAll();}
+
+    public PessoaJuridica buscaPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public PessoaJuridica alterar(Long id, PessoaJuridica alterado) {
+        Optional<PessoaJuridica> encontrado = repository.findById(id);
+        if(encontrado.isPresent()) {
+            PessoaJuridica pessoaJuridica = encontrado.get();
+            pessoaJuridica.setCnpj(alterado.getCnpj());
+            pessoaJuridica.setRazaoSocial(alterado.getRazaoSocial());
+            pessoaJuridica.setTipoEmpresa(alterado.getTipoEmpresa());
+            pessoaJuridica.setNome(alterado.getNome());
+            pessoaJuridica.setEmail(alterado.getEmail());
+            pessoaJuridica.setTelefone(alterado.getTelefone());
+            pessoaJuridica.setEndereco(alterado.getEndereco());
+            return repository.save(pessoaJuridica);
+        }
+        return null;}
+
+    public void remover(Long id) { repository.deleteById(id);}
+
+
+}
