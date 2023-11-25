23a3e5387f8ca6036806cac3abcc802b4a31385f
Erick Monteiro <monteirocgk@gmail.com>
2023-11-25 20:36:47 -0300

    Adicao de regras de negocio na classe Advogado e criacao da classe Processo_Advogado

---
+++ src/main/java/com/advocacia/Advocacia_Beckhauser/models/ProcessoAdvogado.java
@@ -0,0 +1,12 @@
+package com.advocacia.Advocacia_Beckhauser.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_advogado")
+public class ProcessoAdvogado extends EntityID{
+
+    @Column ()
+}
